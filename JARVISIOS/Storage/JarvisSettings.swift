112c9a6cac542fcd9c44c47abab51e70795fbb03
sheldonrocks2022-cmyk <sheldonrocks2022@gmail.com>
2026-09-19 21:17:01 -0500

    Set two minute secure action confirmation window

--- JARVISIOS/Storage/JarvisSettings.swift
+++ JARVISIOS/Storage/JarvisSettings.swift
@@ -1,5 +1,5 @@
 import Foundation
 struct JarvisSettings{
  static var guildID:String{get{UserDefaults.standard.string(forKey:"guild_id") ?? ""}set{UserDefaults.standard.set(newValue,forKey:"guild_id")}}
- static var confirmationTimeout:TimeInterval{60}
+ static var confirmationTimeout:TimeInterval{120}
 }
